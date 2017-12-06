bfc29520a0b31fd6525f3d302b1b62b2a1d0defd
kennethkxf@dingtalk.com <kennethkxf@dingtalk.com>
2017-12-06 18:06:10 +0800

    12

--- app/src/main/java/com/weijing/MainAppcliation.kt
+++ app/src/main/java/com/weijing/MainAppcliation.kt
@@ -1,16 +1,22 @@
 package com.weijing
 
 import android.app.Application
+import android.content.Context
 import android.util.Log
+import com.alibaba.sdk.android.push.CommonCallback
+import com.alibaba.sdk.android.push.noonesdk.PushServiceFactory
 import com.weijing.kotlin.utils.OtherUtils
 
 /**
  * Created by kxf on 2017/10/31.
  */
 class MainAppcliation : Application() {
+    val TAG: String = "MainAppcliation"
+
     override fun onCreate() {
         super.onCreate()
         Log.e("kxf", "MainAppcliation")
+        initCloudChannel(this)
     }
 
     override fun onLowMemory() {
@@ -18,5 +24,25 @@ class MainAppcliation : Application() {
         OtherUtils.isNetWorkConnect(this)
     }
 
+    /**
+     * 初始化云推送通道
+     * @param applicationContext
+     */
+    private fun initCloudChannel(applicationContext: Context) {
+        PushServiceFactory.init(applicationContext)
+        val pushService = PushServiceFactory.getCloudPushService()
+        pushService.register(applicationContext, object : CommonCallback {
+            override fun onSuccess(suc: String?) {
+                Log.d(TAG, "init cloudchannel success : $suc")
+                Log.d(TAG, "${pushService.deviceId}")
+            }
+
+            override fun onFailed(errorCode: String?, errorMessage: String?) {
+                Log.d(TAG, "init cloudchannel failed -- errorcode:$errorCode errorMessage:$errorMessage")
+            }
+
+
+        })
+    }
 
 }
